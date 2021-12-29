9eaf12fbe0399dc30b59c7a3d075620c115718e5
Elektra <ebilali.simou@gmail.com>
2021-12-30 00:05:33 +0200

    checking wakka time

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        asdasda a sdasd asd asd asd asda sd asda sd asda sdasd asd ads asd asd as
+
+        asd ads asd asd a
+        as adas dasda
+        sd asd
+        asd as
         <a
           className="App-link"
           href="https://reactjs.org"
